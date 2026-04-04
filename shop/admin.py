f5bb34445bfe6d43b93926a999ea43b3667db47a
Dayon141 <dastankeunimjaev@gmail.com>
2026-04-04 15:25:45 +0500

    Edit

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Clothes, Brand, Size, Color
 
-admin.site.register(Clothes)
 admin.site.register(Color)
 admin.site.register(Brand)
 admin.site.register(Size)
+
+class ClothesAdmin(admin.ModelAdmin):
+    list_display = ['brand', 'title']
+    list_display_links = ['brand']
+    search_fields = ["brand"]
+    list_filter = ["brand", "size", "color"]
+    list_editable = ["title"]
