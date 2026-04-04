c70e6b08f6161c0535eeef4cb7babd896a8829c6
Dayon141 <dastankeunimjaev@gmail.com>
2026-04-04 15:33:49 +0500

    Edit3

--- shop/admin.py
+++ shop/admin.py
@@ -6,10 +6,10 @@ admin.site.register(Brand)
 admin.site.register(Size)
 
 class ClothesAdmin(admin.ModelAdmin):
-    list_display = ['brand', 'title']
-    list_display_links = ['brand']
-    search_fields = ["brand"]
-    list_filter = ["brand", "size", "color"]
-    list_editable = ["title"]
+    list_display = ['Brand', 'title']
+    list_display_links = ['Brand']
+    search_fields = ['title']
+    list_filter = ['Brand', 'size', 'color']
+    list_editable = ['title']
 
-admin.site.register(Clothes,admin_class = ClothesAdmin)
+admin.site.register(Clothes, admin_class=ClothesAdmin)
